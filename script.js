6f11c534a398a954e85994281106610ad3e3cf21
rayyan-2007 <mrefxeditz@gmail.com>
2026-06-01 09:39:37 +0530

    updated

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-//                 //User Management System:
+                //User Management System:
 
-// // Objective:-
-// // Create a User class and manage multiple users using an array.
+// Objective:-
+// Create a User class and manage multiple users using an array.
 
-// const users = [];
-// class User {
-//   constructor(username, mobilenum, age) {
-//     this.UserName = username;
-//     this.MobileNum = mobilenum;
-//     this.Age = age;
-//   }
+const users = [];
+class User {
+  constructor(username, mobilenum, age) {
+    this.UserName = username;
+    this.MobileNum = mobilenum;
+    this.Age = age;
+  }
 
-//   isAdult() {
-//     if (this.Age >= 18) {
-//         console.log("Adult :",true)
-//       return true;
-//     } else {
-//         console.log("Adult :",false)
-//       return false;
-//     }
-//   }
+  isAdult() {
+    if (this.Age >= 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-//   getUserDetails() {
-//     console.log(`
-// UserName: ${this.UserName},
-// MobileNum: ${this.MobileNum},
-// Age: ${this.Age},`);
-// }
-// }
+  getUserDetails() {
+    console.log(`
+UserName: ${this.UserName},
+MobileNum: ${this.MobileNum},
+Age: ${this.Age},
+//execute the isAdult method of the current object
+Adult: ${this.isAdult()}
+`);
+}
+}
 
-// const userone = new User("alice", "9976786672", 16,);
+const userone = new User("alice", "9976786672", 16,);
 
-// const usertwo = new User("bob", "9976786672",  19, );
+const usertwo = new User("bob", "9976786672",  19, );
 
-// const userthree = new User("charlie", "7418645952",    33,);
+const userthree = new User("charlie", "7418645952",    33,);
 
-// userone.getUserDetails();
-// userone.isAdult(this.Age);
-// usertwo.getUserDetails();
-// usertwo.isAdult(this.Age);
-// userthree.getUserDetails();
-// userthree.isAdult(this.Age);
 
-// users.push(userone);
+users.push(userone,usertwo,userthree);
 
-// users.push(usertwo);
+console.log(users);
 
-// users.push(userthree);
+users.forEach((userdata)=>{
+  userdata.getUserDetails();
+})
 
-// console.log(users);
 
 //Create JSON Data:
 
